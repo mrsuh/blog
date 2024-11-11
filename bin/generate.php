38cf2fa0a06b4a5fd4077cdf3e7e25dd596b94f7
Anton Sukhachev <mrsuh6@gmail.com>
2024-11-11 09:44:56 +0200

    Sort articles

--- bin/generate.php
+++ bin/generate.php
@@ -120,8 +120,6 @@ class MyParserdown extends \Parsedown
             'class' => 'text-muted link-secondary'
         ];
         
-//        var_dump($data);exit;
-        
         return $data;
     }
 
@@ -212,35 +210,34 @@ $sitemap = [
 
 $articles = [
     2024 => [
-        Article::create("SQLite Index Visualization: Search", "/articles/2024/sqlite-index-visualization-search/", "Nov 15"),
         Article::create("SQLite Index Visualization: Structure", "/articles/2024/sqlite-index-visualization-structure/", "Nov 5"),
+        Article::create("SQLite Index Visualization: Search", "/articles/2024/sqlite-index-visualization-search/", "Nov 15"),
     ],
     2023 => [
-        Article::create("How I Wrote PHP Skeleton For Bison", "/articles/2023/how-i-wrote-php-skeleton-for-bison/", "Sep 15"),
-        Article::create("JSON parser with PHP and Bison", "/articles/2023/json-parser-with-php-and-bison/", "Apr 3"),
-        Article::create("Nginx parser with PHP and Bison", "/articles/2023/nginx-parser-with-php-and-bison/", "Mar 27"),
-        Article::create("AST parser with PHP and Bison", "/articles/2023/ast-parser-with-php-and-bison/", "Mar 19"),
-        Article::create("PHP Skeleton for Bison", "/articles/2023/php-skeleton-for-bison/", "Mar 13"),
         Article::create("Few steps to make your docker image smaller", "/articles/2023/few-steps-to-make-your-docker-image-smaller/", "Feb 20"),
+        Article::create("PHP Skeleton for Bison", "/articles/2023/php-skeleton-for-bison/", "Mar 13"),
+        Article::create("AST parser with PHP and Bison", "/articles/2023/ast-parser-with-php-and-bison/", "Mar 19"),
+        Article::create("Nginx parser with PHP and Bison", "/articles/2023/nginx-parser-with-php-and-bison/", "Mar 27"),
+        Article::create("JSON parser with PHP and Bison", "/articles/2023/json-parser-with-php-and-bison/", "Apr 3"),
+        Article::create("How I Wrote PHP Skeleton For Bison", "/articles/2023/how-i-wrote-php-skeleton-for-bison/", "Sep 15"),
     ],
     2022 => [
-        Article::create("PHP generics", "https://phprussia.ru/moscow/2022/abstracts/9165", "Nov 25"),
-        Article::create("How PHP engine builds AST", "/articles/2022/how-php-engine-builds-ast/", "Sep 5"),
-        Article::create("Parsing with PHP, Bison and re2c", "/articles/2022/parsing-with-php-bison-and-re2c/", "Aug 26"),
-        Article::create("Telegram bot that monitors currency availability in Tinkoff ATMs", "https://vc.ru/u/585016-anton-suhachev/393167-telegram-bot-kotoryi-sledit-za-valyutoi-v-bankomatah-tinkoff", "Apr 02"),
-        Article::create("Comparing PHP Collections", "/articles/2022/comparing-php-collections/", "Mar 22"),
         Article::create("Generics implementation approaches", "/articles/2022/generics-implementation-approaches/", "Feb 8"),
+        Article::create("Comparing PHP Collections", "/articles/2022/comparing-php-collections/", "Mar 22"),
+        Article::create("Telegram bot that monitors currency availability in Tinkoff ATMs", "https://vc.ru/u/585016-anton-suhachev/393167-telegram-bot-kotoryi-sledit-za-valyutoi-v-bankomatah-tinkoff", "Apr 02"),
+        Article::create("Parsing with PHP, Bison and re2c", "/articles/2022/parsing-with-php-bison-and-re2c/", "Aug 26"),
+        Article::create("How PHP engine builds AST", "/articles/2022/how-php-engine-builds-ast/", "Sep 5"),
+        Article::create("PHP generics", "https://phprussia.ru/moscow/2022/abstracts/9165", "Nov 25"),
     ],
     2021 => [
         Article::create("PHP Generics . Right here . Right now", "https://habr.com/ru/articles/577750", "Sep 14"),
     ],
     2020 => [
-        Article::create("RC - boat with ESP8266 NodeMCU", "https://habr.com/ru/articles/513482", "Nov 3"),
-        Article::create("RC - car with ESP8266 NodeMCU and LEGO", "https://vc.ru/dev/160142-rc-mashinka-iz-esp8266-nodemcu-i-lego", "Sep 21"),
-        Article::create("Looking for the most interesting articles on the site", "https://vc.ru/dev/159230-ishem-samye-interesnye-stati-v-razdelah-na-saitah-vcru-tjournalru-i-dtfru", "Sep 17"),
         Article::create("How I migrated my hobby project to k8s", "https://habr.com/ru/articles/484528", "Jan 21"),
+        Article::create("Looking for the most interesting articles on the site", "https://vc.ru/dev/159230-ishem-samye-interesnye-stati-v-razdelah-na-saitah-vcru-tjournalru-i-dtfru", "Sep 17"),
+        Article::create("RC - car with ESP8266 NodeMCU and LEGO", "https://vc.ru/dev/160142-rc-mashinka-iz-esp8266-nodemcu-i-lego", "Sep 21"),
+        Article::create("RC - boat with ESP8266 NodeMCU", "https://habr.com/ru/articles/513482", "Nov 3"),
     ],
-
     2019 => [
         Article::create("Comparing PHP FPM, PHP PPM, Nginx Unit, ReactPHP, and RoadRunner", "https://habr.com/ru/articles/431818", "Jan 14"),
     ],
@@ -248,15 +245,14 @@ $articles = [
         Article::create("Mafia with Go, Vanila JS and WebSockets", "https://habr.com/ru/articles/423821", "Oct 5"),
     ],
     2017 => [
-        Article::create("Architecture of a service for collecting and classifying housing listings", "https://habr.com/ru/articles/342220", "Dec 4"),
-        Article::create("Classifying listings from social networks: In search of the best solution", "https://habr.com/ru/articles/328282", "May 14"),
         Article::create("Continuous delivery with Travis CI and Ansible", "https://habr.com/ru/articles/325438", "Apr 3"),
-
+        Article::create("Classifying listings from social networks: In search of the best solution", "https://habr.com/ru/articles/328282", "May 14"),
+        Article::create("Architecture of a service for collecting and classifying housing listings", "https://habr.com/ru/articles/342220", "Dec 4"),
     ],
     2015 => [
-        Article::create("Nginx + Lua + Redis . Efficiently processing sessions and delivering data", "https://habr.com/ru/articles/270463", "Nov 11"),
-        Article::create("SonarQube . Checking code quality", "https://habr.com/ru/articles/259149", "May 29"),
         Article::create("Migration from Symfony 2.0 to 2.6", "https://habr.com/ru/articles/258403", "May 20"),
+        Article::create("SonarQube . Checking code quality", "https://habr.com/ru/articles/259149", "May 29"),
+        Article::create("Nginx + Lua + Redis . Efficiently processing sessions and delivering data", "https://habr.com/ru/articles/270463", "Nov 11"),
     ]
 ];
 
