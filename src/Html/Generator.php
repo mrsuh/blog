fcc448175e8b7a40cfdcd1fdb83540414fb8ece2
Anton Sukhachev <mrsuh6@gmail.com>
2024-11-26 22:15:26 +0200

    Added formatter

--- src/Html/Generator.php
+++ src/Html/Generator.php
@@ -12,9 +12,37 @@ class Generator
     public function generate(string $filePath, string $template, array $context): void
     {
         $template = $this->twig->load($template);
+        $render = $template->render($context);
+
+        $isHtml = str_contains($filePath, '.html');
         file_put_contents(
-            $this->directory . DIRECTORY_SEPARATOR . ltrim($filePath, DIRECTORY_SEPARATOR), 
-            $template->render($context)
+            $this->directory . DIRECTORY_SEPARATOR . ltrim($filePath, DIRECTORY_SEPARATOR),
+            $isHtml ? self::formatHtml($render) : $render
         );
+    }
+
+    private function formatHtml(string $content): string
+    {
+        $tidy = new \tidy();
+
+        $tidy->parseString($content, [
+            'preserve-entities' => true,
+            'hide-comments' => true,
+            'tidy-mark' => false,
+            'wrap' => 200,
+            'wrap-attributes' => false,
+            'break-before-br' => false,
+            'vertical-space' => false,
+
+            'output-xhtml' => false,
+            'output-html' => true,
+
+            'indent' => true,
+            'indent-spaces' => 4,
+        ]);
+
+        $html = $tidy->html();
+
+        return $html->value;
     }
 }
